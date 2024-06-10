44d7b60e3f2c2e24c9f7501a912deb9f902d5cd0
femie <femie.arasu@gmail.com>
2024-06-10 18:10:19 -0500

    Animation Working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
 import './styles/App.css';
 import './styles/general.css';
+import { Suspense } from "react";
+import { Canvas } from '@react-three/fiber';
+//import { OrbitControls } from '@react-three/drei'; after amb light <OrbitControls />
 
+import Scene from './Startswitch.jsx';
 
-function App() {
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>3D Model Viewer</h1>
-
+    <div className= "App">
+      <header className= "App-header">
       </header>
-   
+    
+
+    <Canvas camera= {{fov: 64, position: [-2,2,0]}}>
+      <ambientLight intensity={5} />
+      <Suspense fallback={null}>
+        <Scene />
+      </Suspense>
+    </Canvas>
     </div>
   );
 }
