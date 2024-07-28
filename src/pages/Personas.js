61775ce18b1f3933c8c297931052dcae0326f84a
femie <femie.arasu@gmail.com>
2024-07-28 18:39:12 -0500

    Font changes

---
+++ src/pages/Personas.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Personas() {
+  return(
+
+    <div className= "persona">
+    <div className= "persona-intro">
+      Personas
+    </div>
+    </div>
+
+  );
+}
+
+export default Quizpage;
