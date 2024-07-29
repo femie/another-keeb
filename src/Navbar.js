209f1428872ed43d159c18af8a87f6c9a47a5556
femie <femie.arasu@gmail.com>
2024-07-28 23:24:49 -0500

    added q bank

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,6 @@ export default function Navbar() {
 
   return (
     <div className= "nav-bar">
-    <nav className= "nav">
       <a href="/" className="site-title">
         Keeb Persona
       </a>
@@ -28,7 +27,7 @@ export default function Navbar() {
         </li>
 
       </ul>
-      </nav>
+
       
 
       <Routes>
