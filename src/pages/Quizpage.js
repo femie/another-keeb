0daa15f77bedc200a9a5873d5da130058e21741e
femie <femie.arasu@gmail.com>
2024-07-29 15:29:54 -0500

    Revert "Merge branch 'macbook&desktop' of https://github.com/femie/another-keeb into macbook&desktop"
    This reverts commit 32282ac7b01f7ebb9fd3bdd46256aa45828ead9f, reversing
    changes made to e534011c9e754a1145a6f785f53d4810f822a090.

--- src/pages/Quizpage.js
+++ src/pages/Quizpage.js
@@ -1,57 +1,14 @@
-import React, { useState } from 'react';
-import quizData from '../qz-components/quizData';
+import React from 'react';
 
 function Quizpage() {
+  return(
 
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const currentQuestion = quizData[currentQuestionIndex];
+    <div className= "qz-box">
+    <div className= "qz-intro">
+      This is the Quiz
+    </div>
+    </div>
 
-  const handleAnswerClick = (score) => {
-    console.log('Selected score:', score); // You can process the score here
-    // Move to the next question or handle quiz completion
-    if (currentQuestionIndex < quizData.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      console.log('Quiz completed');
-      // Handle quiz completion logic here
-    }
-  };
-
-  const handlePreviousClick = () => {
-    if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
-    }
-  };
-
-    return(
-      <div>
-        <h2> {currentQuestion.question}</h2>
-        <ul>
-          {currentQuestion.answers.map((answer, index) =>(
-            <li key= {index} onClick={() => handleAnswerClick(answer.score)}> {answer.text}</li>
-          ))}
-        </ul>
-        <button
-          onClick= {handlePreviousClick}
-          disabled= {currentQuestionIndex===0}>
-            Previous
-        </button>
-        <button 
-        onClick={() => {
-          if (currentQuestionIndex < quizData.length - 1) {
-            setCurrentQuestionIndex(currentQuestionIndex + 1);
-          } else {
-            console.log('Quiz completed');
-            // Handle quiz completion logic here
-          }
-        }}
-        disabled={currentQuestionIndex === quizData.length - 1}
-      >
-        Next
-      </button>
-        
-      </div>
-      
   );
 }
 
