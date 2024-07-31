a4b8a228a3aaaf06d62d8395a94a7a07e209b1e3
femie <femie.arasu@gmail.com>
2024-07-31 16:40:21 -0500

    Key materials changed

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App'
 
 function Main() {
+
   return (
+    
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,32 +2,29 @@
 import Model from '../keeb-model/fixed-keeb';
 import React from 'react';
 import { Suspense, useRef } from "react";
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
+
+
 
 function CamSetup(){
   const cameraRef= useRef();
 
-  useFrame(() => {
-    if (cameraRef.current) {
-      cameraRef.current.position.set(0, 2, 0);
-      cameraRef.current.lookAt(0, 0, 0);
-    }
-  });
-
-  return <PerspectiveCamera makeDefault fov= {50} position={[0, 7, 7]}/>;
+  return <PerspectiveCamera makeDefault fov= {50} position={[0, 7, 7]} ref={cameraRef}/>;
 }
+
 
 function Homepage() {
 
+
   return(
 
-  <Canvas style= {{width: '100vw', height: '80vh'}}>
+  <Canvas className= "can" style= {{width: '100vw', height: '80vh'}}>
     <CamSetup/>
-    <ambientLight intensity= {0.5}/> {/* Ambient light to illuminate the scene */}
-    <directionalLight position={[-5, 5, 5]} intensity={3} /> {/* Directional light for shadows and highlights */}
-    <Suspense fallback={null}>
-      <Model position={[0, 6.3, 1]} rotation= {[.6, 4.04, 0]} />
+    <ambientLight intensity= {1}/> {/* Ambient light to illuminate the scene */}
+
+      <Suspense fallback={null}>
+      <Model position={[0,7, 0]} rotation= {[1.01, 4.04, 0]} />
     </Suspense>
 
   </Canvas>
