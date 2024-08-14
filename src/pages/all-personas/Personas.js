0a4735373a481e99b42ee25ba46be082161d21a4
femie <femie.arasu@gmail.com>
2024-08-14 18:03:25 -0500

    routing tweaked again

--- src/pages/all-personas/Personas.js
+++ src/pages/all-personas/Personas.js
@@ -2,33 +2,33 @@ import React from 'react';
 import '../../styles/allpersonas.css';
 import { Outlet, NavLink } from "react-router-dom";
 
+
 function Personas() {
 
 
   return(
 
     <div className="personas-container">
-     {/* Persona Menu & Routing */}
+
+    {/* Persona Menu */}
 
      <div className= "persona-menu">
-     <h3 className="persona-menu-title">
-       All Personas
-     </h3>
+      <h3 className= "persona-menu-title">All Personas</h3>
 
      <ul>
        <li>
          <NavLink to="istp">ISTP</NavLink>
+         
        </li>
 
-
      </ul>
+     </div>
+     <Outlet/>
 
-</div>
+  </div>
 
-    <div className="persona-content">
-      <Outlet />
-    </div>
-</div>
+    
+
 
   );
 }
--- src/pages/all-personas/persona-pages/istp-page.js
+++ src/pages/all-personas/persona-pages/istp-page.js
@@ -5,11 +5,12 @@ import '../../../styles/istp-styles.css';
 function istp() {
   return(
 
-  
+    
+  <div className="istp-container">
     <h3 className= "istp-keeb">
       This is your Keyboard - Ergonomic
     </h3>
-
+    </div>
 
   );
 }
